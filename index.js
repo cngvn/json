ee425b9f98a087a3f981e725eb1bbef62d78726f
chinguun <cngvn@G305-CC0057.local>
2024-10-29 17:21:37 +0800

    ff

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "DELETE") {
-
+    let body = "";
+    req.on("data", (data) => {
+      body += data;
+    });
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
   }
 });
 server.listen(8080, () => console.log("Server is running"));
